da3ae5c3d5cd2686d7de18a048287603dd006701
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2023-06-02 20:30:47 +0100

    Add philosophers example
    Signed-off-by: Agustín Díaz <agustin.ramiro.diaz@gmail.com>

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,7 +1,4 @@
-fn main() {
-    println!("Check the tests :D");
-    println!("cargo test -- --show-output");
-}
+use rand::Rng;
 
 #[test]
 fn shared_counter() {
@@ -47,3 +44,38 @@ fn shared_counter() {
 // let (tx, rx) = channel();
 // tx.send(()).unwrap();
 // rx.recv().unwrap();
+
+fn main() {
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+    use std::time;
+
+    const philosophers: usize = 3;
+
+    let forks = [0; philosophers].map(|_| Arc::new(Mutex::new(())));
+
+    (0..philosophers)
+        .map(|thread_index| {
+            let left: Arc<Mutex<()>> = forks[thread_index].clone();
+            let right = forks[(thread_index + 1) % philosophers].clone();
+
+            thread::spawn(move || loop {
+                thread::sleep(time::Duration::from_secs(
+                    rand::thread_rng().gen_range(0..5),
+                ));
+
+                println!("Philosopher {thread_index} waiting to pick up left");
+                let _left = left.lock().unwrap();
+
+                thread::sleep(time::Duration::from_secs_f64(rand::random::<f64>()));
+
+                println!("Philosopher {thread_index} waiting to pick up right");
+                let _right = right.lock().unwrap();
+
+                println!("Philosopher {thread_index} dropping");
+            })
+        })
+        .collect::<Vec<_>>() // we collect it here so it's not lazy
+        .into_iter()
+        .for_each(|h| h.join().unwrap());
+}

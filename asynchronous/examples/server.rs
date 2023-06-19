a80716547c453d87c9118d351734f017e64175f1
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2023-06-19 17:59:01 -0300

    Create TCP server with tokio
    Signed-off-by: Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>

---
+++ asynchronous/examples/server.rs
@@ -0,0 +1,32 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:5000").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        println!("Client connected");
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(mut socket: TcpStream) {
+    let mut buf = [0; 1000];
+    socket.read(&mut buf).await.unwrap();
+
+    println!("A client has sent {}", String::from_utf8_lossy(&buf));
+
+    socket
+        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+        .await
+        .unwrap();
+    socket.flush().await.unwrap();
+}

f08a169a6763eee322524d628e391ebce5b697c6
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2023-06-22 09:49:08 -0300

    Add more examples
    Signed-off-by: Agustín Díaz <agustin.ramiro.diaz@gmail.com>

--- memory-management/examples/ownership.rs
+++ memory-management/examples/ownership.rs
@@ -6,13 +6,34 @@ fn moving_ownership() {
 
 fn borrowing() {
     let mut owner = "data".to_string();
+    // Multiple immutable (read-only) references
     let watcher = &owner;
     let second_watcher = &owner;
 
-    watcher.push_str("!");
+    watcher.push_str("!"); // cannot mutate immutable reference
 
-    let mutable_reference = &mut owner;
+    let mutable_reference = &mut owner; // first mutable reference
     mutable_reference.push_str("!");
+}
+
+fn either_one_mutable_or_many_inmutables() {
+    let mut owner = "data".to_string();
+    // Multiple immutable (read-only) references
+    let watcher = &owner;
+    let second_watcher = &owner;
+
+    let mutable_reference = &mut owner; // first mutable reference
+    mutable_reference.push_str("!");
+
+    println!("{} {} {}", watcher, second_watcher, mutable_reference);
+}
+
+fn only_one_mutable() {
+    let mut owner = "data".to_string();
+    let first_mutable_reference = &mut owner;
+    let second_mutable_reference = &mut owner; // second mutable reference is not allowed
+
+    println!("{} {}", first_mutable_reference, second_mutable_reference);
 }
 
 fn dangling_reference(_input: &String) -> &String {
